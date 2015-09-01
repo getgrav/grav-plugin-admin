deb8af52eff9ee4150a92c59da5211093dd810ca
vivalldi <cosgrove.tyler.98@gmail.com>
2015-09-01 17:04:09 -0400

    update mobile.js for overlay

--- themes/grav/js/mobile.js
+++ themes/grav/js/mobile.js
@@ -11,11 +11,20 @@ $(document).ready(function(){
     //mobile-only actions
     if($(window).width() / parseFloat($("body").css("font-size"))<mobile_only) {
         var admin_sidebar = document.getElementById("admin-sidebar");
+        var overlay = document.createElement("div");
+            overlay.id = "overlay";
+            overlay.style.position = 'fixed';
+            overlay.style.width = '25%';
+            overlay.style.height = '100%';
+            overlay.style.zIndex = 999999;
+            overlay.style.left = '75%';
+        document.body.getElementsByClassName('remodal-bg')[0].appendChild(overlay);
+        document.getElementById('overlay').style.display = 'none';
         //add listener to titlebar to acivate sidebar
         document.getElementById("titlebar").addEventListener("click",function(){
             //open sidebar
             $(admin_sidebar).toggle("slide");
-            console.log("toggle");
+            document.getElementById('overlay').style.display = 'inherit';
             var selected = admin_sidebar.getElementsByClassName("selected")[0].getElementsByTagName("a");
             selected[0].href="javascript:void(0)";
         });
@@ -24,8 +33,13 @@ $(document).ready(function(){
         admin_sidebar.addEventListener("click", function(event){
             if(event.target == admin_sidebar || event.target == selected[0]){
                 $(admin_sidebar).toggle("slide");
-                console.log("clicker1");
+                document.getElementById('overlay').style.display = 'none';
             }
+        });
+        document.getElementById('overlay').addEventListener("click", function(event){
+            $(admin_sidebar).toggle("slide");
+            document.getElementById('overlay').style.display = 'none';
         });
     }
 });
+
