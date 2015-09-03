88e946804ae5ec52d324d395fdb34acd7b52571f
Tyler Cosgrove <Vivalldi@users.noreply.github.com>
2015-09-03 16:45:40 -0400

    Menu bug hotfix

--- themes/grav/js/mobile.js
+++ themes/grav/js/mobile.js
@@ -7,9 +7,9 @@ $(document).ready(function(){
     var mobile_only= large_desktop_container - 0.062;
     var no_mobile= tablet_container;
     var small_mobile_range= large_mobile_container;
+    
+    var menu_state = "closed";
 
-    //mobile-only actions
-    if($(window).width() / parseFloat($("body").css("font-size"))<mobile_only) {
         var admin_sidebar = document.getElementById("admin-sidebar");
         var overlay = document.createElement("div");
             overlay.id = "overlay";
@@ -20,26 +20,40 @@ $(document).ready(function(){
             overlay.style.left = '75%';
         document.body.getElementsByClassName('remodal-bg')[0].appendChild(overlay);
         document.getElementById('overlay').style.display = 'none';
+        var selected = admin_sidebar.getElementsByClassName("selected")[0].getElementsByTagName("a");
         //add listener to titlebar to acivate sidebar
         document.getElementById("titlebar").addEventListener("click",function(){
-            //open sidebar
-            $(admin_sidebar).toggle("slide");
-            document.getElementById('overlay').style.display = 'inherit';
-            var selected = admin_sidebar.getElementsByClassName("selected")[0].getElementsByTagName("a");
-            selected[0].href="javascript:void(0)";
+            if($(window).width() / parseFloat($("body").css("font-size"))<mobile_only && menu_state=="closed") {
+                //open sidebar
+                console.log(menu_state);
+                $(admin_sidebar).toggle("slide");
+                document.getElementById('overlay').style.display = 'inherit';
+                selected[0].href="javascript:void(0)";
+                menu_state = "open";
+                console.log(menu_state);
+            }
         });
 
         //enable sidebar closing;
         admin_sidebar.addEventListener("click", function(event){
-            if(event.target == admin_sidebar || event.target == selected[0]){
-                $(admin_sidebar).toggle("slide");
-                document.getElementById('overlay').style.display = 'none';
+            if($(window).width() / parseFloat($("body").css("font-size"))<mobile_only && menu_state == "open") {
+                if(event.target == admin_sidebar || event.target == selected[0]){
+                    console.log(menu_state);
+                    $(admin_sidebar).toggle("slide");
+                    document.getElementById('overlay').style.display = 'none';
+                    menu_state = "closed";
+                    console.log(menu_state);
+                }
             }
         });
         document.getElementById('overlay').addEventListener("click", function(event){
-            $(admin_sidebar).toggle("slide");
-            document.getElementById('overlay').style.display = 'none';
+            if($(window).width() / parseFloat($("body").css("font-size"))<mobile_only && menu_state == "open") {
+                console.log(menu_state);
+                $(admin_sidebar).toggle("slide");
+                document.getElementById('overlay').style.display = 'none';
+                menu_state = "closed";
+                console.log(menu_state);
+            }
         });
-    }
 });
 
