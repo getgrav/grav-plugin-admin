970d77219e4eac80c413045c87dcfaefd61fefb8
Flavio Copes <copesc@gmail.com>
2015-11-25 18:31:25 +0100

    Fixes https://github.com/getgrav/grav/issues/476, replace default themes service with admin own theme service to avoid loading the default theme

--- admin.php
+++ admin.php
@@ -111,6 +111,12 @@ class AdminPlugin extends Plugin
             // Disable Asset pipelining
             $this->config->set('system.assets.css_pipeline', false);
             $this->config->set('system.assets.js_pipeline', false);
+
+            // Replace themes service with admin.
+            $this->grav['themes'] = function ($c) {
+                require_once __DIR__ . '/classes/themes.php';
+                return new Themes($this->grav);
+            };
         }
 
         // We need popularity no matter what
