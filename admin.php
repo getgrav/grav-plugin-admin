28073f128a1ab255ab1ef121947cc784ea0b1368
Tyler Cosgrove <cosgrove.tyler.98@gmail.com>
2016-01-24 17:18:16 -0500

    Dynamic Route for theme

--- admin.php
+++ admin.php
@@ -418,7 +418,7 @@ class AdminPlugin extends Plugin
         $twig->twig_vars['admin_route'] = trim($this->config->get('plugins.admin.route'), '/');
         $twig->twig_vars['base_url_relative'] =
             $twig->twig_vars['base_url_simple'] . '/' . $twig->twig_vars['admin_route'];
-        $twig->twig_vars['theme_url'] = '/user/plugins/admin/themes/' . $this->theme;
+        $twig->twig_vars['theme_url'] = str_replace(GRAV_ROOT,"",str_replace('\\','/',__DIR__)) . '/themes/' . $this->theme;
         $twig->twig_vars['base_url'] = $twig->twig_vars['base_url_relative'];
         $twig->twig_vars['base_path'] = GRAV_ROOT;
         $twig->twig_vars['admin'] = $this->admin;
