6c92563e05450a30577bc847cd0454f3badb97be
Flavio Copes <copesc@gmail.com>
2016-03-07 11:34:08 +0100

    Handle installing a plugin and its dependencies

--- themes/grav/app/utils/packages.js
+++ themes/grav/app/utils/packages.js
@@ -4,11 +4,11 @@ import request from '../utils/request';
 
 class Packages {
 
-    getBackToList(type) {
+    static getBackToList(type) {
         window.location.href = `${config.base_url_relative}/${type}s`;
     }
 
-    addDependencyToList(type, dependency, slug = '') {
+    static addDependencyToList(type, dependency, slug = '') {
         let container = $('.package-dependencies-container');
         let text = `${dependency} <a href="#" class="button" data-dependency-slug="${dependency}" data-package-action="remove-dependency-${type}">Remove</a>`;
 
@@ -21,7 +21,7 @@ class Packages {
 
     addDependenciesToList(dependencies, slug = '') {
         dependencies.forEach((dependency) => {
-            this.addDependencyToList('plugin', dependency, slug);
+            Packages.addDependencyToList('plugin', dependency, slug);
         });
     }
 
@@ -33,7 +33,7 @@ class Packages {
         this.removePackage('theme', slug);
     }
 
-    getRemoveUrl(type) {
+    static getTaskUrl(type, task) {
         var url = `${config.base_url_relative}`;
 
         if (type === 'plugin') {
@@ -42,18 +42,38 @@ class Packages {
             url += `/themes.json`;
         }
 
-        url += `/task${config.param_sep}removePackage`;
+        url += `/task${config.param_sep}${task}`;
 
         return url;
     }
 
+    static getRemovePackageUrl(type) {
+        return `${Packages.getTaskUrl(type, 'removePackage')}`;
+    }
+
+    static getAddPackageUrl(type) {
+        return `${Packages.getTaskUrl(type, 'addPackage')}`;
+    }
+
+    static getPackageDependenciesUrl(type) {
+        return `${Packages.getTaskUrl(type, 'packageDependencies')}`;
+    }
+
+    static getInstallDependenciesOfPackageUrl(type) {
+        return `${Packages.getTaskUrl(type, 'installDependenciesOfPackage')}`;
+    }
+
+    static getInstallPackageUrl(type) {
+        return `${Packages.getTaskUrl(type, 'installPackage')}`;
+    }
+
     removePackage(type, slug) {
-        let url = this.getRemoveUrl(type);
+        let url = Packages.getRemovePackageUrl(type);
 
         request(url, {
             method: 'post',
             body: {
-                plugin: slug
+                package: slug
             }
         }, (response) => {
             if (response.status == 'success') {
@@ -67,20 +87,20 @@ class Packages {
                 }
 
                 //The package was removed. When the modal closes, move to the packages list
-                $(document).on('closing', '[data-remodal-id="delete-package"]', (e) => {
-                    this.getBackToList(type);
+                $(document).on('closing', '[data-remodal-id="delete-package"]', () => {
+                    Packages.getBackToList(type);
                 });
             }
         });
     }
 
     removeDependency(type, slug, button) {
-        let url = this.getRemoveUrl(type);
+        let url = Packages.getRemovePackageUrl(type);
 
         request(url, {
             method: 'post',
             body: {
-                plugin: slug
+                package: slug
             }
         }, (response) => {
             if (response.status == 'success') {
@@ -90,6 +110,89 @@ class Packages {
                 if (response.dependencies.length > 0) {
                     this.addDependenciesToList(response.dependencies, slug);
                 }
+            }
+        });
+    }
+
+    addNeededDependencyToList(type, dependency) {
+        $('.install-dependencies-package-container .type-' + type).removeClass('hidden');
+        let list = $('.install-dependencies-package-container .type-' + type + ' ul');
+        let text = `${dependency}`;// <a href="#" class="button" data-dependency-slug="${dependency}" data-package-action="remove-dependency-${type}">Remove</a>`;
+        list.append(`<li>${text}</li>`);
+    }
+
+    getPackageDependencies(type, slug, finishedLoadingCallback) {
+        let url = Packages.getPackageDependenciesUrl(type);
+
+        //check dependencies
+        request(url, {
+            method: 'post',
+            body: {
+                package: slug
+            }
+        }, (response) => {
+
+            finishedLoadingCallback();
+
+            if (response.status == 'success') {
+                if (response.dependencies) {
+                    var hasDependencies = false;
+                    for (var dependency in response.dependencies) {
+                        if (response.dependencies.hasOwnProperty(dependency)) {
+                            hasDependencies = true;
+                            let dependencyName = dependency;
+                            let dependencyType = response.dependencies[dependency];
+
+                            this.addNeededDependencyToList(dependencyType, dependencyName);
+                        }
+                    }
+
+                    if (hasDependencies) {
+                        $('.install-dependencies-package-container').removeClass('hidden');
+                    } else {
+                        //can install right away
+                        alert('has NO dep');
+                    }
+                }
+            }
+        });
+    }
+
+    installDependenciesOfPackage(type, slug, callbackSuccess, callbackError) {
+        let url = Packages.getInstallDependenciesOfPackageUrl(type);
+
+        //check dependencies
+        request(url, {
+            method: 'post',
+            body: {
+                package: slug
+            }
+        }, (response) => {
+
+            if (response.status == 'success') {
+                callbackSuccess();
+            } else {
+                callbackError();
+            }
+        });
+    }
+
+    installPackage(type, slug, callbackSuccess, callbackError) {
+        let url = Packages.getInstallPackageUrl(type);
+
+        //check dependencies
+        request(url, {
+            method: 'post',
+            body: {
+                package: slug,
+                type: type
+            }
+        }, (response) => {
+
+            if (response.status == 'success') {
+                callbackSuccess();
+            } else {
+                callbackError();
             }
         });
     }
