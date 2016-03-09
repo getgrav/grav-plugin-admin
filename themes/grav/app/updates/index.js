7c3b39022aeb48fbc8f61441d2f07f0f838db4ac
Flavio Copes <copesc@gmail.com>
2016-03-09 18:08:23 +0100

    Use the add package form for updating too

--- themes/grav/app/updates/index.js
+++ themes/grav/app/updates/index.js
@@ -111,7 +111,7 @@ export default class Updates {
                     <p>
                         <i class="fa fa-bullhorn"></i>
                         <strong>v${resources[item].available}</strong> ${translations.PLUGIN_ADMIN.OF_THIS} ${singles[index]} ${translations.PLUGIN_ADMIN.IS_NOW_AVAILABLE}!
-                        <a href="${config.base_url_relative}/${type}/${item}/${task}update/admin-nonce${config.param_sep}${config.admin_nonce}" class="button button-small secondary">${translations.PLUGIN_ADMIN.UPDATE} ${singles[index].charAt(0).toUpperCase() + singles[index].substr(1).toLowerCase()}</a>
+                        <a href="#" class="button button-small secondary" data-remodal-target="add-package" data-package-slug="${item}" data-${singles[index]}-action="get-package-dependencies">${translations.PLUGIN_ADMIN.UPDATE} ${singles[index].charAt(0).toUpperCase() + singles[index].substr(1).toLowerCase()}</a>
                     </p>
                     `);
                 }
