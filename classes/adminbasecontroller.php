6a1ee7481ac7ed4cf803d62143185660b741b941
Flavio Copes <copesc@gmail.com>
2017-01-12 19:55:43 +0100

    Remove unnneded code, related to https://github.com/getgrav/grav-plugin-admin/commit/ca00a9b150856526d773e9c5a2d1e36c6f0dcb05

--- classes/adminbasecontroller.php
+++ classes/adminbasecontroller.php
@@ -805,11 +805,6 @@ class AdminBaseController
         $type      = $uri->param('type');
         $field     = $uri->param('field');
 
-        $event = $this->grav->fireEvent('onAdminCanSave', new Event(['controller' => &$this]));
-        if (isset($event['can_save']) && $event['can_save'] == false) {
-            return false;
-        }
-
         $this->taskRemoveMedia();
 
         if ($type == 'pages') {
