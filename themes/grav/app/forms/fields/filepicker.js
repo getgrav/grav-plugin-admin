5b30a3d83c1465c4c31841486fce96042c4ba98d
Djamil Legato <djamil@djamil.it>
2017-05-19 13:02:26 -0700

    Cleanup and loading thumbs from cache when available

--- themes/grav/app/forms/fields/filepicker.js
+++ themes/grav/app/forms/fields/filepicker.js
@@ -2,6 +2,8 @@ import $ from 'jquery';
 import { config } from 'grav-config';
 import request from '../../utils/request';
 
+const insertTextAt = (string, index, text) => [string.slice(0, index), text, string.slice(index)].join('');
+
 export default class FilePickerField {
 
     constructor(options) {
@@ -34,6 +36,7 @@ export default class FilePickerField {
         let field = (isInput ? element : element.find('input, select'));
 
         let folder = '';
+        let thumbs = {};
 
         if (!field.length || field.get(0).selectize) { return; }
 
@@ -52,19 +55,16 @@ export default class FilePickerField {
                 }
 
                 let data = [];
+                thumbs = response.thumbs || {};
 
                 for (let i = 0; i < response.files.length; i++) {
-                    if (mode === 'selected' && response.files[i] !== value) {
-                        continue;
-                    }
-                    data.push({ 'name': response.files[i], 'status': 'available' });
+                    if (mode === 'selected' && response.files[i] !== value) { continue; }
+                    data.push({ 'name': response.files[i], 'status': 'available', thumb: thumbs[response.files[i]] || '' });
                 }
 
                 for (let i = 0; i < response.pending.length; i++) {
-                    if (mode === 'selected' && response.pending[i] !== value) {
-                        continue;
-                    }
-                    data.push({ 'name': response.pending[i], 'status': 'pending' });
+                    if (mode === 'selected' && response.pending[i] !== value) { continue; }
+                    data.push({ 'name': response.pending[i], 'status': 'pending', thumb: thumbs[response.pending[i]] || '' });
                 }
 
                 folder = response.folder;
@@ -78,17 +78,14 @@ export default class FilePickerField {
         let renderOption = function renderOption(item, escape) {
             let image = '';
             if (imagesPreview && folder && (!item.status || item.status === 'available') && item.name.match(/\.(jpg|jpeg|png|gif)$/i)) {
-                var insertTextInStringAt = function insertTextInStringAt(string, index, text) {
-                    return [string.slice(0, index), text, string.slice(index)].join('');
-                };
-
-                let fallback1 = insertTextInStringAt(`${config.base_url_relative}/../${folder}/${item.name}`, -4, '@2x');
-                let fallback2 = insertTextInStringAt(`${config.base_url_relative}/../${folder}/${item.name}`, -4, '@3x');
+                const fallback2x = insertTextAt(`${config.base_url_relative}/../${folder}/${item.name}`, -4, '@2x');
+                const fallback3x = insertTextAt(`${config.base_url_relative}/../${folder}/${item.name}`, -4, '@3x');
+                const source = thumbs[item.name] || `${config.base_url_relative}/../${folder}/${item.name}`;
 
                 image = `
                     <img class="filepicker-field-image"
-                         src="${config.base_url_relative}/../${folder}/${item.name}"
-                         onerror="if(this.src=='${fallback1}'){this.src='${fallback2}';this.onerror='';}else{this.src='${fallback1}'};" />`;
+                         src="${source}"
+                         onerror="if(this.src==='${fallback2x}'){this.src='${fallback3x}';this.onerror='';}else{this.src='${fallback2x}'}" />`;
             }
 
             return `<div>
@@ -109,7 +106,7 @@ export default class FilePickerField {
             optgroupField: 'status',
             // lockOptgroupOrder: true,
             create: false,
-            preload: true, // 'focus',
+            preload: false, // 'focus',
             render: {
                 option: function(item, escape) {
                     return renderOption(item, escape);
