e7b71ae5309bab9a55ab3049a35973f8173824d7
Ryan Boog <ryan@hdwebpros.com>
2018-03-09 14:15:43 -0600

    Updated unset.js. Fixed quote style

--- themes/grav/app/pages/page/unset.js
+++ themes/grav/app/pages/page/unset.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 $(document).on('click', '.dz-unset', function() {
     $(this).closest('.dz-image-preview').remove();
     var unsetimage = $(this).closest('.dz-image-preview').find('img').attr('src');
-    var images = $('.files-upload').find('input[data-grav-field=\'hidden\']').val();
+    var images = $('.files-upload').find('input[data-grav-field="hidden"]').val();
     var imagearray = {};
     $.each(JSON.parse(images), function(ind, obj) {
         if ('/' + ind !== unsetimage) {
