b36917756765a7664ea7adc0b3a99796588a4c79
Djamil Legato <djamil@djamil.it>
2018-03-09 18:21:37 -0800

    Fixed couple issues with the unset of images

--- themes/grav/app/pages/page/unset.js
+++ themes/grav/app/pages/page/unset.js
@@ -2,13 +2,14 @@ import $ from 'jquery';
 
 $(document).on('click', '.dz-unset', function() {
     $(this).closest('.dz-image-preview').remove();
-    var unsetimage = $(this).closest('.dz-image-preview').find('img').attr('src');
-    var images = $('.files-upload').find('input[data-grav-field="hidden"]').val();
-    var imagearray = {};
-    $.each(JSON.parse(images), function(ind, obj) {
-        if ('/' + ind !== unsetimage) {
-            imagearray[ind] = obj;
+    const file_upload = $('.files-upload');
+    const unset_image = $(this).closest('.dz-image-preview').find('[data-dz-name]').text().trim();
+    const images = JSON.parse(file_upload.find('input[data-grav-field="hidden"]').val()) || {};
+    let image_array = {};
+    $.each(images, function(ind, obj) {
+        if (!ind.endsWith(unset_image)) {
+            image_array[ind] = obj;
         }
     });
-    $('.files-upload').find('input[data-grav-field="hidden"]').val(JSON.stringify(imagearray));
+    file_upload.find('input[data-grav-field="hidden"]').val(JSON.stringify(image_array));
 });
