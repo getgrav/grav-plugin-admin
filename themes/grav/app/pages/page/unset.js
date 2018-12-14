54d800a5ed30a7b934b140ab71cafc0431bbdefe
Ryan Boog <ryan@hdwebpros.com>
2018-12-14 16:21:24 -0600

    Fixed unset so it only targets nearest hidden field, not all hidden fields

--- themes/grav/app/pages/page/unset.js
+++ themes/grav/app/pages/page/unset.js
@@ -1,15 +1,19 @@
 import $ from 'jquery';
 
 $(document).on('click', '.dz-unset', function() {
-    $(this).closest('.dz-image-preview').remove();
-    const file_upload = $('.files-upload');
-    const unset_image = $(this).closest('.dz-image-preview').find('[data-dz-name]').text().trim();
-    const images = JSON.parse(file_upload.find('input[data-grav-field="hidden"]').val()) || {};
-    let image_array = {};
-    $.each(images, function(ind, obj) {
-        if (!ind.endsWith(unset_image)) {
-            image_array[ind] = obj;
-        }
-    });
-    file_upload.find('input[data-grav-field="hidden"]').val(JSON.stringify(image_array));
+    const file_upload = $(this).closest('.files-upload');
+    const next_closest = $(this).closest('.dz-image-preview');
+    const hidden_field = 'input[data-grav-field="hidden"]';
+    if (next_closest.length) {
+        const unset_image = next_closest.find('[data-dz-name]').text().trim();
+        const images = JSON.parse(file_upload.find(hidden_field).val()) || null;
+        let image_array = {};
+        $.each(images, function(ind, obj) {
+            if (!ind.endsWith(unset_image)) {
+                image_array[ind] = obj;
+            }
+        });
+        file_upload.find(hidden_field).val(JSON.stringify(image_array));
+        next_closest.remove();
+    }
 });
