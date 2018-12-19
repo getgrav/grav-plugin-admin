3feadb7bd9776d240f552c7f818f04d04f285bbd
Andy Miller <rhuk@mac.com>
2018-12-18 20:18:01 -0700

    Unset state from user if no super or user admin

--- classes/admin.php
+++ classes/admin.php
@@ -702,6 +702,7 @@ class Admin
         // Clean field for users who shouldn't be able to modify these fields
         if (!$this->authorize(['admin.user', 'admin.super'])) {
             unset($post['access']);
+            unset($post['state']);
         }
 
         return $post;
