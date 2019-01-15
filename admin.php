03a06486432033aee6cd4783a7437a840b0a8703
Raphaël Droz <raphael.droz+floss@gmail.com>
2019-01-14 22:02:19 -0300

    Added `admin.tools` permission (#1550)
    So that an administrator can disable access to `Direct Install of Grav Packages`

--- admin.php
+++ admin.php
@@ -862,6 +862,7 @@ class AdminPlugin extends Plugin
             'admin.statistics'    => 'boolean',
             'admin.plugins'       => 'boolean',
             'admin.themes'        => 'boolean',
+            'admin.tools'         => 'boolean',
             'admin.users'         => 'boolean',
         ];
         $admin->addPermissions($permissions);
