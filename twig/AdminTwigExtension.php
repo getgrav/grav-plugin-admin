8649d8ab8aba20f09fd13aa6c2d27e5c9f717772
Matias Griese <matias@trilbymedia.com>
2019-05-31 14:51:44 +0300

    Added deprecation message if another plugin is not autoloading AdminTwigExtension

--- twig/AdminTwigExtension.php
+++ twig/AdminTwigExtension.php
@@ -3,4 +3,5 @@
 
 require_once __DIR__ . '/../classes/plugin/Twig/AdminTwigExtension.php';
 
+user_error("require_once('user/plugins/admin/twig/AdminTwigExtension.php') is deprecated. All admin classes are using composer autoloader.", E_USER_DEPRECATED);
 class_alias('Grav\Plugin\Admin\Twig\AdminTwigExtension', 'Grav\Plugin\Admin\AdminTwigExtension');
