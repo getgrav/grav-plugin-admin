cb61a000880adb468bea56de7db1ba2cf288e0c8
Matias Griese <matias@trilbymedia.com>
2019-05-31 14:45:11 +0300

    Update deprecated class to use the new file location

--- twig/AdminTwigExtension.php
+++ twig/AdminTwigExtension.php
@@ -1,6 +1,6 @@
 <?php
 // Deprecated.
 
-require_once __DIR__ . '/../classes/Twig/AdminTwigExtension.php';
+require_once __DIR__ . '/../classes/plugin/Twig/AdminTwigExtension.php';
 
 class_alias('Grav\Plugin\Admin\Twig\AdminTwigExtension', 'Grav\Plugin\Admin\AdminTwigExtension');
