e9d83115ae8f37a1b217e8e01a9d2200a5aa9102
Djamil Legato <djamil@djamil.it>
2019-06-08 17:38:17 -0700

    Add node type class name to the finder and pathbar node items

--- themes/grav/app/utils/finderjs.js
+++ themes/grav/app/utils/finderjs.js
@@ -279,6 +279,7 @@ class Finder {
             listItemClasses.push(this.config.className[this.config.childKey]);
         }
 
+        listItemClasses.push(`fjs-item-${item.type}`);
         listItem.addClass(listItemClasses.join(' '));
         listItem.append(link)
             .attr('data-fjs-item', item[this.config.itemKey]);
@@ -299,7 +300,7 @@ class Finder {
             const isLast = (index + 1) === activeItems.length;
             itemKeys += `/${item[this.config.itemKey]}`;
             this.pathBar.append(`
-                <span class="breadcrumb-node" ${item.type === 'dir' ? `data-breadcrumb-node="${itemKeys}"` : ''}>
+                <span class="breadcrumb-node breadcrumb-node-${item.type}" ${item.type === 'dir' ? `data-breadcrumb-node="${itemKeys}"` : ''}>
                     <i class="fa fa-fw ${this.getIcon(item.type)}"></i>
                     <span class="breadcrumb-node-name">${$('<div />').text(item[this.config.labelKey]).html()}</span>
                     ${!isLast ? '<i class="fa fa-fw fa-chevron-right"></i>' : ''}
