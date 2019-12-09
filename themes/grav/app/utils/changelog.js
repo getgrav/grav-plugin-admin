3f75fef0d8c37794f38b051441f999e65c4638f2
Andy Miller <rhuk@mac.com>
2019-12-09 11:29:41 -0700

    simple loader

--- themes/grav/app/utils/changelog.js
+++ themes/grav/app/utils/changelog.js
@@ -9,6 +9,7 @@ $(document).on('click', '[data-remodal-changelog]', (event) => {
 
 $(document).on('opened', '[data-remodal-id="changelog"]', () => {
     const instance = $.remodal.lookup[$('[data-remodal-id=changelog]').data('remodal')];
+    instance.$modal.html('<div class="changelog-overflow center" style="padding:5rem 0;text-align:center;"><i class="fa fa-spinner fa-spin fa-3x fa-fw"></i></div>');
     if (!TRIGGER) { return true; }
 
     const url = $(TRIGGER).data('remodalChangelog');
