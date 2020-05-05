06ecacb4a2357c359efb1e54ac899c40b3c10c49
Andy Miller <rhuk@mac.com>
2020-05-05 11:29:41 -0600

    started on horizontal scroller for theme previews

--- themes/grav/app/whitelabel/presets.js
+++ themes/grav/app/whitelabel/presets.js
@@ -38,7 +38,9 @@ body.on('click', '[data-preset-values]', (event) => {
     let data = target.data('preset-values');
 
     Object.keys(data).forEach((section) => {
-        if (typeof data[section] === 'string') { return; }
+        if (typeof data[section] === 'string') {
+            return;
+        }
 
         Object.keys(data[section]).forEach((key) => {
             let field = $(`[name="data[whitelabel][color_scheme][${section}][${key}]"], [name="data[${section}][${key}]"]`);
@@ -61,4 +63,108 @@ body.on('click', '[data-reset-scss]', (event) => {
         setField($(`[name="${field}"]`), value);
     });
     fields = [];
+});
+
+// Horizontal Scroll Functionality
+$.fn.hscrollarrows = function() {
+    return this.each(function() {
+
+        let navNext = $('<a class="nav-next hide"></a>');
+        let navPrev = $('<a class="nav-prev hide"></a>');
+        let scrollTime = null;
+        let resizeTime = null;
+        let scrolling = false;
+
+        let elm_w = 0;
+        let elem_data_w = 0;
+        let max_scroll = 0;
+        let inc_scroll = 0;
+
+        let calcData = function() {
+            elm_w = elem.width();
+            elem_data_w = elem_data.width();
+            max_scroll = elem_data_w - elm_w;
+            inc_scroll = elm_w * 0.3; // 20%
+        };
+
+        let revalidate = function() {
+            calcData();
+            stateNavs();
+        };
+
+        let run = function() {
+            calcData();
+            setupNavs();
+        };
+
+        let setupNavs = function() {
+
+            elem.parent().prepend(navNext);
+            elem.parent().prepend(navPrev);
+            navNext.on('click', next);
+            navPrev.on('click', prev);
+            stateNavs();
+
+            $(elem).scroll(function() {
+                if (!scrolling) {
+                    clearTimeout(scrollTime);
+                    scrollTime = setTimeout(function() {
+                        stateNavs();
+                    }, 250);
+                }
+            });
+
+            $(window).resize(function() {
+                clearTimeout(resizeTime);
+                resizeTime = setTimeout(function() {
+                    revalidate();
+                }, 250);
+            });
+        };
+
+        let stateNavs = function() {
+            let current_scroll = elem.scrollLeft;
+            if (current_scroll < max_scroll) {
+                navNext.removeClass('hide');
+            } else {
+                navNext.addClass('hide');
+            }
+            if (current_scroll > 0) {
+                navPrev.removeClass('hide');
+            } else {
+                navPrev.addClass('hide');
+            }
+            scrolling = false;
+        };
+
+        let next = function() {
+            let current_scroll = elem.scrollLeft;
+            if (current_scroll < max_scroll) {
+                scrolling = true;
+                elem.stop().animate({
+                    scrollLeft: (current_scroll + inc_scroll)
+                }, stateNavs);
+            }
+        };
+
+        let prev = function() {
+            let current_scroll = elem.scrollLeft;
+            if (current_scroll > 0) {
+                scrolling = true;
+                elem.stop().animate({
+                    scrollLeft: (current_scroll - inc_scroll)
+                }, stateNavs);
+            }
+        };
+
+        let elem = $(this);
+        let elem_data = $(':first-child', elem);
+
+        run();
+
+    });
+};
+
+$(document).ready(() => {
+    $('.jquery-horizontal-scroll').hscrollarrows();
 });
