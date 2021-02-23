3f91679d96f908b7d859cd3f41543f84a9e9e3e8
Djamil Legato <djamil+github@djamil.it>
2021-02-23 10:07:18 -0800

    Faster computation of direction

--- themes/grav/app/forms/fields/multilevel.js
+++ themes/grav/app/forms/fields/multilevel.js
@@ -11,12 +11,9 @@ $(function() {
             // top
             the_name = 'data-attr-name="' + name + '"';
         }
-        var $body = $(document.body);
-        if ($body.css('direction') == 'rtl' ){
-            var marginDir = 'margin-right';
-        } else {
-            var marginDir = 'margin-left';
-        }
+        
+        const marginDir = window.getComputedStyle(document.body).direction === 'ltr' ? 'margin-left' : 'margin-right';
+        
         let field = `
             <div class="element-wrapper">
                 <div class="form-row array-field-value_only js__multilevel-field ${top}"
