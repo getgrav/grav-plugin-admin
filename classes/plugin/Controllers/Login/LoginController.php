58e2c6cc55a42a65d486920859a41e6aa4d5aa06
Matias Griese <matias@trilbymedia.com>
2021-03-31 11:48:15 +0300

    Change nonce expiration message

--- classes/plugin/Controllers/Login/LoginController.php
+++ classes/plugin/Controllers/Login/LoginController.php
@@ -133,7 +133,7 @@ class LoginController extends AdminController
         try {
             $this->checkNonce();
         } catch (PageExpiredException $e) {
-            $this->setMessage($e->getMessage(), 'error');
+            $this->setMessage($this->translate('PLUGIN_ADMIN.INVALID_SECURITY_TOKEN'), 'error');
 
             return $this->createDisplayResponse();
         }
@@ -218,7 +218,7 @@ class LoginController extends AdminController
         try {
             $this->checkNonce();
         } catch (PageExpiredException $e) {
-            $this->setMessage($e->getMessage(), 'error');
+            $this->setMessage($this->translate('PLUGIN_ADMIN.INVALID_SECURITY_TOKEN'), 'error');
 
             return $this->createDisplayResponse();
         }
@@ -255,7 +255,7 @@ class LoginController extends AdminController
         try {
             $this->checkNonce();
         } catch (PageExpiredException $e) {
-            $this->setMessage($e->getMessage(), 'error');
+            $this->setMessage($this->translate('PLUGIN_ADMIN.INVALID_SECURITY_TOKEN'), 'error');
 
             return $this->createDisplayResponse();
         }
@@ -318,7 +318,7 @@ class LoginController extends AdminController
         try {
             $this->checkNonce();
         } catch (PageExpiredException $e) {
-            $this->setMessage($e->getMessage(), 'error');
+            $this->setMessage($this->translate('PLUGIN_ADMIN.INVALID_SECURITY_TOKEN'), 'error');
 
             return $this->createDisplayResponse();
         }
@@ -394,7 +394,7 @@ class LoginController extends AdminController
         try {
             $this->checkNonce();
         } catch (PageExpiredException $e) {
-            $this->setMessage($e->getMessage(), 'error');
+            $this->setMessage($this->translate('PLUGIN_ADMIN.INVALID_SECURITY_TOKEN'), 'error');
 
             return $this->createDisplayResponse();
         }
@@ -513,7 +513,7 @@ class LoginController extends AdminController
         try {
             $this->checkNonce();
         } catch (PageExpiredException $e) {
-            $this->setMessage($e->getMessage(), 'error');
+            $this->setMessage($this->translate('PLUGIN_ADMIN.INVALID_SECURITY_TOKEN'), 'error');
 
             return $this->createDisplayResponse();
         }
